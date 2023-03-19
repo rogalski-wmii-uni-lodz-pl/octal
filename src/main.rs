12072cd0afa99384efb41280769a3897208c0053
Mark Rogalski <marek.rogalski@wmii.uni.lodz.pl>
2023-03-19 10:18:11 +0100

    Save skipped nimbers into files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
 // use game::gen_rares;
 use std::env;
+use std::fs;
+use std::io::{Read, Write};
+use std::path::Path;
 use std::time::Instant;
 
 pub mod octal;
+
+fn save(n: usize, rules_str: &str, buf: &[u8]) {
+    let path = format!("nimbers_{}_{}", rules_str, n);
+
+    fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(path)
+        .unwrap()
+        .write(buf)
+        .unwrap();
+}
+
+fn load(max: usize, path: &Path) -> Vec<octal::Nimber> {
+    println!("Reading nimbers from {:?}", path);
+
+    let nimber_bytes = (octal::Nimber::BITS / u8::BITS) as usize;
+    let mut nimbers = vec![0 as octal::Nimber; max];
+    let mut buf: Vec<u8> = Vec::with_capacity(max * nimber_bytes);
+
+    fs::OpenOptions::new()
+        .read(true)
+        .open(path)
+        .unwrap()
+        .read_to_end(&mut buf)
+        .unwrap();
+
+    for i in 0..max {
+        let mut n: octal::Nimber = 0;
+        for b in 0..nimber_bytes {
+            let loc = (i * nimber_bytes) + (nimber_bytes - b) - 1;
+            n += (buf[loc] as octal::Nimber) << (b * nimber_bytes);
+        }
+        nimbers[i] = n;
+    }
+    nimbers
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -44,9 +84,43 @@ fn main() {
     println!("total: {:?}", start.elapsed());
 
     if !period_found && max_tail_memory != 0 {
-        g.nimbers.copy_to_g_back();
+        let mut last = 0;
 
-        for n in max_full_memory.. {
+        for i in (max_full_memory..).step_by(max_tail_memory) {
+            let p = format!("nimbers_{}_{}", rules_str, i);
+            let path = Path::new(&p);
+
+            if path.exists() {
+                last = i;
+            } else {
+                break;
+            }
+        }
+
+        if last == 0 {
+            last = max_full_memory;
+            g.nimbers.copy_to_g_back();
+        } else {
+            let p = format!("nimbers_{}_{}", rules_str, last);
+            let path = Path::new(&p);
+            let loaded = load(max_tail_memory, path);
+            g.nimbers.g_back = loaded;
+        }
+
+        let nimber_bytes = (octal::Nimber::BITS / u8::BITS) as usize;
+        let mut buf: Vec<u8> = vec![0; max_tail_memory * nimber_bytes];
+
+        for n in last.. {
+            if n % max_tail_memory == 0 {
+                for i in 0..max_tail_memory {
+                    let nim = g.nimbers.g_back[i];
+                    for b in 0..nimber_bytes {
+                        let loc = (i * nimber_bytes) + (nimber_bytes - b) - 1;
+                        buf[loc] = (nim >> b * nimber_bytes) as u8;
+                    }
+                }
+                save(n, rules_str, &buf)
+            }
             g.calc_rc_back(n);
             g.occasional_info_back(n, &start);
         }
